32e86ada0b94a2a17642a0c152466f5b40107c2d
Famuwagun Oluwatofunmi <tofunmifamu@gmail.com>
2024-01-22 08:34:38 -0800

    fix dor task 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
+
 def safe_print_integer(value):
-    """
-    Prints an integer using "{:d}".format().
+    """Print an integer with "{:d}".format().
 
     Args:
-        value: The value to be printed.
+        value (int): The integer to print.
 
     Returns:
-        True if the value has been correctly printed (integer), False otherwise.
+        If a TypeError or ValueError occurs - False.
+        Otherwise - True.
     """
     try:
         print("{:d}".format(value))
         return (True)
-    except (ValueError, TypeError):
+    except (TypeError, ValueError):
         return (False)
