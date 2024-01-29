5e6e9314f8d5b577ba10b7cf967a9bfd4d5d6815
Famuwagun Oluwatofunmi <tofunmifamu@gmail.com>
2024-01-29 13:39:43 -0800

    fix for task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,6 +6,12 @@ class Rectangle:
     """This represents the Rectangle"""
 
     def __init__(self, width=0, height=0):
+         """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
         self.width = width
         self.height = height
 
