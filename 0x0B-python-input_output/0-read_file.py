f33179d82db8c72c1799ec79ffdfd8603d68d0d3
Famuwagun Oluwatofunmi <tofunmifamu@gmail.com>
2024-02-06 13:15:40 -0800

    task 0 correction

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-this function contains the read_file
+Contains the read_file function
 """
 
 
 def read_file(filename=""):
-    """""this reads a text file(UTF8) and prints it to stdout"""
+    """""reads a text file(UTF8) and prints it to stdout"""
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
     
