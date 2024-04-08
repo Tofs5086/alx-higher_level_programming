2915c5d30a98c63ff71ee4ae9414f91e8aa0996c
Famuwagun Oluwatofunmi <tofunmifamu@gmail.com>
2024-04-08 14:45:42 -0700

    javascrpt is cool

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,15 +1,3 @@
 #!/usr/bin/node
-const argumentCount = process.argv.length;
-
-if (argumentCount === 2)
-{
-	console.log('No argument');
-}
-else if (argumentCount === 3)
-{
-	console.log('Argument found');
-}
-else
-{
-	console.log('Arguments found');
-}
+const args = process.argv.length;
+console.log(args === 2 ? 'No argument' : args === 3 ? 'Argument found' : 'Arguments found');
--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,12 +1,3 @@
 #!/usr/bin/node
-const argument = process.argv.slice(2);
-
-if (argument.length === 0) 
-{
-    console.log('No argument');
-} 
-else 
-{
-    console.log(argument[0]);
-}
+console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
 
