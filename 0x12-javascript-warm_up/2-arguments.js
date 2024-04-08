81178b95528a7992e4d4d66ef53249bedfe8f239
Famuwagun Oluwatofunmi <tofunmifamu@gmail.com>
2024-04-08 14:09:06 -0700

    arguments in javascript

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const argumentCount = process.argv.length;
+
+if (argumentCount === 2)
+{
+	console.log('No argument');
+}
+else if (argumentCount === 3)
+{
+	console.log('Argument found');
+}
+else
+{
+	console.log('Argument found');
+}
