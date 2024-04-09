9ea645ff76d5022eb061ba47cc01f664b29bac20
Famuwagun Oluwatofunmi <tofunmifamu@gmail.com>
2024-04-09 14:57:09 -0700

    rectangle

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 class Rectangle {
-	rectangle(w, h) {
-		this.width = w;
-		this.height = h;
-	}
+  constructor(w, h) {
+    this.width = w;
+    this.height = h;
+  }
 }
 
 module.exports = Rectangle;
+
