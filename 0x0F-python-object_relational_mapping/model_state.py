116381b79c1d866eeafbff6f70d5ca1432eeeb81
Famuwagun Oluwatofunmi <tofunmifamu@gmail.com>
2024-04-15 22:02:43 +0100

    python ORM

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
-from sqlalchemy import Column, Integer, String, create_engine
+"""
+Contains State class and Base, an instance of declarative_base()
+"""
+from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
-"""Define declarative base"""
-Base = declarative_base()
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
 
 class State(Base):
+    """
+    Class with id and name attributes of each state
+    """
     __tablename__ = 'states'
-    
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-
-"""Establish connection to MySQL server"""
-engine = create_engine('mysql://username:password@localhost:3306/database_name')
-
-"""Create all the tables"""
-Base.metadata.create_all(engine)
-
-"""Bind a sessionmaker"""
-Session = sessionmaker(bind=engine)
-session = Session()
 
