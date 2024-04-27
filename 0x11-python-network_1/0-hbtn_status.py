9028739348b74ea2b2ef21b1812e2c0310d5b04c
Famuwagun Oluwatofunmi <tofunmifamu@gmail.com>
2024-04-27 13:13:08 +0100

    python network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
+"""
 
-"""Script to fecth alx-intranet.hbtn"""
-import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
+if __name__ == '__main__':
+    import urllib.request
 
-with urllib.request.urlopen(url) as response:
-    body_response = response.read()
-    utf8_content = body_response.decode('utf-8')
-
-print("Body response:")
-print("\t- type:", type(body_response))
-print("\t- content:", body_response)
-print("\t- utf8 content:", utf8_content)
-
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
