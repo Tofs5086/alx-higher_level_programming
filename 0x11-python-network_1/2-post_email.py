a0b6787961c43f45ae0b65b3e28dc5fbb81a1cd7
Famuwagun Oluwatofunmi <tofunmifamu@gmail.com>
2024-04-27 13:26:58 +0100

    post email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""A script that:
+- sends a POST request to a URL with an email parameter
+- encodes the email as a parameter
+- displays the body of the respose
+"""
+import sys
+import urllib.parse
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
